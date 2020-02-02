786cd706eb16db241ebf5d62f05c4063a990436c
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2020-02-02 16:15:28 -0500

    pickedup function

--- contracts/Shipment.sol
+++ contracts/Shipment.sol
@@ -1,34 +1,42 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.5.12;
 
 contract Carrier {
 
 address owner;
+mapping(address => uint256) public balanceOf; 
 
-struct shipment {
-    string shipmentid;
-    string locationA;
-    string locationB;
+struct Shipment {
+    ///string shipmentid;
+    ///string locationA;
+    ///string locationB;
     bool pickedup;
     bool delivered;
-    uint256 amount;
+    ///uint256 amount;
 }
+
+mapping (address => Shipment) public ShipmentInfo;
 
 struct carrier {
     uint DOTnumber;
     address carrier;
 }
 
+event Deposit (
+    address indexed _from,
+    bytes32 indexed _id,
+    uint _value
+);
+
   constructor() public {
       owner = msg.sender;
   }
-
+    
   function deposit(uint256 amount) payable public {
         require(msg.value == amount);
-
-  function completedShipment(uint shipmentamount) payable public {
-      if (pickedup = 1 && delivered = 1) {
-          then deposit(shipmentamount);
-      }
+        balanceOf[msg.sender] += amount;  
   }
-
-}
+  
+  function setShipment(bool pickedstatus) public {
+        Shipment storage shipment = ShipmentInfo[msg.sender];
+        shipment.pickedup = pickedstatus;
+    }
